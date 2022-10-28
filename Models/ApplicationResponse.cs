3a3bf01ed6c5cb9e4af1b98d3240f63ac73d913a
kbruner4 <kelliebruner22@gmail.com>
2022-10-27 22:10:15 -0600

    seeding data

--- Models/ApplicationResponse.cs
+++ Models/ApplicationResponse.cs
@@ -22,8 +22,60 @@ namespace BaconsaleMovieCollection.Models
         public string Rating { get; set; }
         public bool Edited { get; set; }
         public string LentTo { get; set; }
-        [MaxLength(25)]
         public string Notes { get; set; }
+        [Required]
+        public string FirstName { get; set;}
+        [Required]
+        public string LastName { get; set;}
+        [Required]
+        public string International { get; set;}
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Hours { get; set;}
+        [Required]
+        public string Semester { get; set; }
+        [Required]
+        public string Phone { get; set;}
+        [Required]
+        public int BYUID { get; set; }
+        [Required]
+        public string Position { get; set; }
+        [Required]
+        public int ClassCode { get; set;}
+        [Required]
+        public int EmpRecord { get; set;}
+        [Required]
+        public string Supervisor { get; set;}
+        [Required]
+        public string HireDate { get; set;}
+        [Required]
+        public float PayRate { get; set;}
+        public string LastPayIncrease { get; set;}
+        public float PayIncreaseAmount { get; set;}
+        public string IncreaseInputDate { get; set;}
+        [Required]
+        public string YearInProgram { get; set;}
+        [Required]
+        public string PayGradTutition { get; set;}
+        public string Terminated { get; set;}
+        public string TerminationDate { get; set;}
+        [Required]
+        public string QualtricsSurveySent { get; set;}
+        [Required]
+        public string SubmittedEform { get; set;}
+        [Required]
+        public string FormSubmissionDate { get; set;}
+        [Required]
+        public string AuthorizationToWork { get; set;}
+        [Required]
+        public string AuthorizationEmailDate { get; set;}
+        [Required]
+        public string BYUName { get; set;}
+        [Required]
+        public string NameChangeCompleted { get; set;}
 
         // Foreign key relationship
         [Required]
